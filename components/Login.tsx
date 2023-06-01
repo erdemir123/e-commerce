fe21b4278ece29b05c23829c96083c3b1acf0b00
erdemir123 <sadikerdemir93@gmail.com>
2023-06-02 00:25:41 +0300

    cypress e2e test added

--- components/Login.tsx
+++ components/Login.tsx
@@ -2,16 +2,16 @@
 
 import { signIn } from "next-auth/react";
 import Image from "next/image";
-import { AiOutlineShoppingCart } from "react-icons/ai";
 const Login = () => {
   return (
-    <div className="bg-[#11a37f] h-screen flex flex-col items-center justify-center text-center ">
+    <div className="bg-[#11a37f] h-screen flex flex-col items-center justify-center text-center " id="login">
       <Image
         src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSofGI77Cz3tXbKTReFuM-Gqlp88zXT4Rgm6A&usqp=CAU"
         width={300}
         height={300}
         alt="logo"
         className="mb-4 rounded-xl shadow-xl shadow-slate-600"
+        id="img"
       />
       <button
         onClick={() => signIn("google")}
---
+++ cypress/e2e/spec.cy.ts
@@ -0,0 +1,21 @@
+describe("template spec", () => {
+  it("passes", () => {
+    cy.visit("");
+    cy.contains("Login To See Discounts and Buy").click();
+
+    cy.get("button").click()
+    cy.request('GET', '/api/auth/signin/google').then((response) => {
+      expect(response.status).to.equal(200);
+    });
+    cy.get("#login").should(
+      "have.css",
+      "background-color",
+      "rgb(17, 163, 127)"
+    );
+    cy.get("#img").should(
+      "have.attr",
+      "src",
+      "/_next/image?url=https%3A%2F%2Fencrypted-tbn0.gstatic.com%2Fimages%3Fq%3Dtbn%3AANd9GcSofGI77Cz3tXbKTReFuM-Gqlp88zXT4Rgm6A%26usqp%3DCAU&w=640&q=75"
+    );
+  });
+});
